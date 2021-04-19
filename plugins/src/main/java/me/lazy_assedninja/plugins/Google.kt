0cd4a2aa6dc130aec3c9713f8a55d45eef65ea9e
Lazy-assed Ninja <henryhuang861219@gmail.com>
2021-04-19 11:44:32 +0800

    remove: dagger-android SDK

--- plugins/src/main/java/me/lazy_assedninja/plugins/Google.kt
+++ plugins/src/main/java/me/lazy_assedninja/plugins/Google.kt
@@ -4,9 +4,6 @@ object Google {
     val material = "com.google.android.material:material:${DependenciesVersions.googleMaterial}"
     val easyPermission = "pub.devrel:easypermissions:${DependenciesVersions.easyPermission}"
     val dagger = "com.google.dagger:dagger:${DependenciesVersions.dagger}"
-    val daggerAndroid = "com.google.dagger:dagger-android:${DependenciesVersions.dagger}"
     val daggerCompiler = "com.google.dagger:dagger-compiler:${DependenciesVersions.dagger}"
-    val daggerAndroidProcessor =
-        "com.google.dagger:dagger-android-processor:${DependenciesVersions.dagger}"
     val gson = "com.google.code.gson:gson:${DependenciesVersions.gson}"
 }
