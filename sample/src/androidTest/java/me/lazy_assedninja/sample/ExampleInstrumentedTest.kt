2c8f3db95b71382b0f9324897a5e9be58cf8391c
Lazy-assed Ninja <henryhuang861219@gmail.com>
2021-05-22 05:03:04 +0800

    fix: Instrumented Tests bugs
    Package name.

--- sample/src/androidTest/java/me/lazy_assedninja/sample/ExampleInstrumentedTest.kt
+++ sample/src/androidTest/java/me/lazy_assedninja/sample/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("me.lazy_assedninja.sample", appContext.packageName)
+        assertEquals("me.lazy_assedninja.demo", appContext.packageName)
     }
 }
