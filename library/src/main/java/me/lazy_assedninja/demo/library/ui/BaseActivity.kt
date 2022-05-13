68a5d6591d55a77016ad7c73d710d5bb4ff2a6e0
Henry <henryhuang861219@gmail.com>
2022-05-13 13:53:36 +0800

    add: lifecycle related log & util

--- library/src/main/java/me/lazy_assedninja/demo/library/ui/BaseActivity.kt
+++ library/src/main/java/me/lazy_assedninja/demo/library/ui/BaseActivity.kt
@@ -1,15 +1,100 @@
 package me.lazy_assedninja.demo.library.ui
 
 import android.content.Context
+import android.os.Bundle
 import android.os.IBinder
+import android.os.PersistableBundle
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenCreated
+import androidx.lifecycle.whenResumed
+import androidx.lifecycle.whenStarted
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+private const val TAG_LIFECYCLE_CALLBACKS = "ActivityLifecycle"
 
 /**
  * A generic AppCompatActivity that provide generic functions.
  */
 @Suppress("unused")
 abstract class BaseActivity : AppCompatActivity() {
+
+    /**
+     * Lifecycle
+     */
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        printLifecycleCallbacks("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        printLifecycleCallbacks("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        printLifecycleCallbacks("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        printLifecycleCallbacks("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        printLifecycleCallbacks("onStop")
+    }
+
+
+    override fun onRestart() {
+        super.onRestart()
+
+        printLifecycleCallbacks("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        printLifecycleCallbacks("onDestroy")
+    }
+
+    /**
+     * Util
+     */
+    private fun printLifecycleCallbacks(callbacks: String) = Timber.tag(TAG_LIFECYCLE_CALLBACKS).i("${this.javaClass.simpleName} - $callbacks")
+
+    private fun setLifecycleScope(
+        beforeCreated: () -> Unit = {},
+        created: () -> Unit = {},
+        started: () -> Unit = {},
+        resumed: () -> Unit = {},
+        afterResumed: () -> Unit = {}
+    ) = lifecycleScope.launch {
+        beforeCreated()
+
+        whenCreated {
+            created()
+        }
+
+        whenStarted {
+            started()
+        }
+
+        whenResumed {
+            resumed()
+        }
+
+        afterResumed()
+    }
 
     protected fun dismissKeyboard(windowToken: IBinder?) {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
