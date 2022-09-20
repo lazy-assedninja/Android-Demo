1b1fc2bdf2229f1a2a6f10935270017105d57ff7
Henry <henryhuang861219@gmail.com>
2022-09-20 15:20:17 +0800

    update: remove useless build tools version config
     if use AGP 3.0+, project will automatically use default version of SDK build tools.

--- plugins/src/main/java/me/lazy_assedninja/plugins/ConfigVersions.kt
+++ plugins/src/main/java/me/lazy_assedninja/plugins/ConfigVersions.kt
@@ -6,11 +6,10 @@ package me.lazy_assedninja.plugins
 @Suppress("unused")
 object ConfigVersions {
 
-    const val COMPILE_SDK_VERSION = 31
-    const val BUILD_TOOLS_VERSION = "30.0.3"
+    const val COMPILE_SDK_VERSION = 33
     const val MIN_LIBRARY_SDK_VERSION = 18
     const val MIN_APP_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 31
+    const val TARGET_SDK_VERSION = 33
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
 }
